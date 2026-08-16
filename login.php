50d5e13f050471b3d031e24581bbd3be00db0070
Sandy Gomez <sgomezm2002@alumno.ipn.mx>
2026-08-15 23:11:09 -0600

    ICO

--- login.php
+++ login.php
@@ -69,7 +69,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>SICE | Iniciar Sesión</title>
+  <title>SICE | </title>
   <link rel="icon" href="img/SICEICO2.ico" type="image/x-icon">
   <link href="./css/bootstrap.min.css" rel="stylesheet" />
   <link href="./css/styles.css" rel="stylesheet" />
